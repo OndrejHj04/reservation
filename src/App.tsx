3cdae88b5a1e075cda72605947f3c6c6bd794a9d
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-06-13 19:18:02 +0200

    Patch[1.4]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { sign } from "crypto";
+import { Navbar } from "./Navbar";
 import { useEffect, useReducer } from "react";
-import {logIn, logOut} from "./Types"
+import {logIn, logOut, initial, state} from "./Types"
 import {SignIn}  from "./SignIn"
-const initial = { data: { user: { displayName: "", email: "", photoURL: "" } } };
 
-const reducer = (state: { data: {} }, action: logIn | logOut) => {
+const reducer = (state: { data: state  }, action: logIn | logOut) => {
   switch (action.type) {
     case "sign":
       localStorage.setItem("user", JSON.stringify(action.data));
@@ -24,12 +23,7 @@ export const App = () => {
   return (
     <>
       {state.data.user.photoURL !== "" ? (
-        <div>
-          <h1>welcome {state.data.user.displayName}</h1>
-          <h1>your email: {state.data.user.email}</h1>
-          <img className="rounded-full" src={state.data.user.photoURL!} alt="" />
-          <button onClick={() => dispatch({ type: "out" })}>log out</button>
-        </div>
+        <Navbar state={state} dispatch={dispatch}/>
       ) : (
         <SignIn dispatch={dispatch} />
       )}
--- src/Types.tsx
+++ src/Types.tsx
@@ -1,6 +1,8 @@
+export const initial = { data: { user: { displayName: "", email: "", photoURL: "" } } };
+export type state = { user: { displayName: string | null; email: string | null; photoURL: string | null } }
 export type logIn = {
   type: "sign";
-  data: { user: { displayName: string | null; email: string | null; photoURL: string | null } };
+  data: state;
 };
 export type logOut = {
   type: "out";
